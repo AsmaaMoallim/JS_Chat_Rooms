e17853b10778d6f8fa6dd1b2122e52ff9f1e40ef
Asmaa Moallim <41068564+AsmaaMoallim@users.noreply.github.com>
2022-08-03 10:50:11 +0300

    test ui.js

--- public/scripts/ui.js
+++ public/scripts/ui.js
@@ -9,6 +9,7 @@ export class ChatUI {
   }
 
   render(data) {
+    console.log("I work (render)")
     const when = dateFns.distanceInWordsToNow(data.created_at.toDate(), {
       addSuffix: "ago",
     });
