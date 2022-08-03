471fdf7320a9aea1b510cfcc215f22965141146d
Asmaa Moallim <41068564+AsmaaMoallim@users.noreply.github.com>
2022-08-03 08:52:31 +0300

    .

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -1,5 +1,5 @@
-import { Chatroom } from "/chat.js";
-import { ChatUI } from "/ui.js";
+import { Chatroom } from "/scripts/chat.js";
+import { ChatUI } from "/scripts/ui.js";
 
 // DOM queries
 const chatList = document.querySelector(".chat-list");
